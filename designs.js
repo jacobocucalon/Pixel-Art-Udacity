c7aba09200cd05a9709b179ff54c36241ea752f0
jacobocucalon <84005590+jacobocucalon@users.noreply.github.com>
2021-05-11 19:07:54 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -12,7 +12,7 @@ var vGrid = document.getElementById("pixelCanvas");
 // function for creating the matrix
 function makeGrid() {
 //clear matrix
-  vGrid.innerHTML = ""; //(attribution: https://knowledge.udacity.com/questions/561177 (11/06/2021))
+  vGrid.innerHTML = ""; //(attribution: https://knowledge.udacity.com/questions/561177 (11/05/2021))
 //loop that creates the rows
   let i = 0;
   while (i < vHeight.value) {
@@ -27,7 +27,7 @@ function makeGrid() {
       cubicles.style.backgroundColor = "black";
       cubicles.style.borderColor="white";
       // insert color listener to each cell so they change color when clicked
-      cubicles.addEventListener("click", function(){ //(attribution: https://knowledge.udacity.com/questions/297130)(11/06/2021)
+      cubicles.addEventListener("click", function(){ //(attribution: https://knowledge.udacity.com/questions/297130)(11/05/2021)
         cubicles.style.backgroundColor = vColorPicker.value;
       });
       j = j + 1;
@@ -38,6 +38,6 @@ function makeGrid() {
 // Prevent refresh and Call Matrix Function when Submit is clicked
 vSizePicker.addEventListener('submit', function(GenerateWhenSubmit) {
   //avoid page refreshing when submit button is clicked
-  GenerateWhenSubmit.preventDefault(); //(attribution: https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault)(11/06/2021)
+  GenerateWhenSubmit.preventDefault(); //(attribution: https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault)(11/05/2021)
   makeGrid(vHeight,vWidth);
   });
